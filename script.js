734b770201ffda175927b7d3aad8f7457de068ab
AlessandroPFreitas <164437174+AlessandroPFreitas@users.noreply.github.com>
2025-03-06 09:50:59 -0300

    Att-Organização do codigo

--- script.js
+++ script.js
@@ -1,79 +1,98 @@
 const weatherForm = document.querySelector("#wrapper__search");
 const searchInput = document.querySelector(".search__input");
-const wrapperCard = document.querySelector(".wrapper__card");
+const card = document.querySelector(".wrapper__card");
 const apiKey = "e7325174f9aa3b353b164cc0a011707a";
-let weatherCondition = document.querySelector(".card__emoji");
-let searchInputValue = "";
 
 weatherForm.addEventListener("submit", async (event) => {
   event.preventDefault();
-  searchInputValue = searchInput.value;
+  const searchInputValue = searchInput.value;
 
-  try {
-    const API = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${searchInputValue}&appid=${apiKey}&units=metric`
-    );
-    const data = await API.json();
-    dataApi(data);
-  } catch (error) {
-    console.log(error);
+  if (searchInputValue) {
+    try {
+      const weatherData = await getWeatherData(searchInputValue);
+      displayData(weatherData);
+      console.log(weatherData);
+    } catch (error) {
+      console.error(error);
+      displayError(error);
+    }
+  } else {
+    console.error(error);
+    displayError("Digite uma cidade/pais/estado!");
   }
 });
 
-function dataApi(data) {
-wrapperCard.classList.add("visible");
-wrapperCard.style.backgroundColor = "";
+async function getWeatherData(searchInputValue) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInputValue}&appid=${apiKey}&units=metric`;
+  const response = await fetch(apiUrl);
+  if (!response.ok) {
+    throw new Error("This city does not exist");
+  }
+  return response.json();
+}
 
-  let errorMessage = document.getElementById("error-message");
-  let displayCard = document.querySelector(".card");
-  if (data.cod === "404" || searchInputValue == "") {
-    displayCard.classList.remove("visible")
-    wrapperCard.style.backgroundColor = "black";
-    errorMessage.textContent = "Este pais/cidade/estado não existe!";
-    errorMessage.classList.remove("hidden");
-  } else {
-    displayCard.classList.add("visible")
-    errorMessage.classList.add("hidden");
-    const name = data.name;
-    const temp = Math.floor(data.main.temp);
-    const humid = data.main.humidity;
-    const weather = data.weather[0].main;
+function displayData(data) {
+  const {
+    name: city,
+    main: { temp, humidity },
+    weather: [{ id }],
+  } = data;
 
-    const processedDada = {
-      name,
-      temp,
-      humid,
-      weather,
-    };
+  card.textContent = "";
+  card.classList.add("visible");
 
-    displayData(processedDada);
+  const cityDisplay = document.createElement("h1");
+  const forecastDisplay = document.createElement("div");
+  const tempDisplay = document.createElement("p");
+  const weatherDisplay = document.createElement("p");
+  const humidDisplay = document.createElement("p");
+
+  cityDisplay.textContent = city;
+  tempDisplay.textContent = `${temp.toFixed(1)}°C`;
+  humidDisplay.textContent = `${humidity}%`;
+  weatherDisplay.textContent = displayEmoji(id);
+  console.log(id);
+
+  card.appendChild(cityDisplay);
+  card.appendChild(forecastDisplay);
+  forecastDisplay.appendChild(weatherDisplay);
+  forecastDisplay.appendChild(tempDisplay);
+  card.appendChild(humidDisplay);
+
+  forecastDisplay.classList.add("card__forecast");
+  cityDisplay.classList.add("card__name");
+  tempDisplay.classList.add("card__temp");
+  weatherDisplay.classList.add("card__emoji");
+  humidDisplay.classList.add("card__humid");
+}
+
+function displayEmoji(weatherData) {
+  switch (true) {
+    case weatherData >= 200 && weatherData < 300:
+      return "⛈️";
+    case weatherData >= 300 && weatherData < 400:
+      return "🌧️";
+    case weatherData >= 500 && weatherData < 600:
+      return "🌧️";
+    case weatherData >= 600 && weatherData < 700:
+      return "❄️";
+    case weatherData >= 700 && weatherData < 800:
+      return "🌪️";
+    case weatherData == 800:
+      return "☀️";
+    case weatherData >= 801 && weatherData < 810:
+      return "☁️";
+      default:
+        return "❓";
   }
 }
 
-function displayData(weatherData) {
-  document.querySelector(".card__name").textContent = weatherData.name;
-  document.querySelector(
-    ".card__temp"
-  ).innerHTML = `${weatherData.temp}<span class="temp__celsius">°C</span>`;
-  document.querySelector(
-    ".card__humid"
-  ).textContent = `Humidity: ${weatherData.humid}%`;
+function displayError(error) {
+  const errorDisplay = document.createElement("p");
+  errorDisplay.textContent = error;
+  errorDisplay.id = "error-message";
 
-  switch (weatherData.weather) {
-    case "Thunderstorm":
-      weatherCondition.textContent = "⛈️";
-      break;
-    case "Rain":
-      weatherCondition.textContent = "🌧️";
-      break;
-    case "Snow":
-      weatherCondition.textContent = "🌨️";
-      break;
-    case "Clear":
-      weatherCondition.textContent = "☀️";
-      break;
-    case "Clouds":
-      weatherCondition.textContent = "☁️";
-      break;
-  }
+  card.textContent = "";
+  card.classList.add("visible");
+  card.prepend(errorDisplay);
 }
